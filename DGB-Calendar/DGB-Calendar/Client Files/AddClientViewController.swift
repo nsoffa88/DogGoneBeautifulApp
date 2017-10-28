eef6f768afc01f7d8fa090a34903200521cdedf1
Nicholas Soffa <zaramath88@gmail.com>
2017-10-27 17:02:31 -0700

    Minor File Management and Client Files Set Up
    Moved around files into proper folders. Set up View Controllers and class files for the client portion of the app.

---
+++ DGB-Calendar/DGB-Calendar/Client Files/AddClientViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddClientViewController.swift
+//  DGB-Calendar
+//
+//  Created by Nicholas Soffa on 10/27/17.
+//  Copyright © 2017 Nicholas Soffa. All rights reserved.
+//
+
+import UIKit
+
+class AddClientViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
